5bd4c44cb5bce842d44bd6e1a53cf09495d1adae
tandingming <2084954083.@qq.com>
2022-05-13 23:50:16 +0800

    Initial commit

---
+++ src/main/java/com/mytest/Array/Array.java
@@ -0,0 +1,16 @@
+package com.mytest.Array;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : TanDingMing
+ * @version :
+ * @Project : algorithm
+ * @Package : com.mytest.Array
+ * @ClassName : .java
+ * @createTime : 2022/5/13 23:49
+ * @Email :
+ * @Description :
+ */
+public class Array {
+}
